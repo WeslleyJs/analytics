055c71ba2566b5d405d687171e1f9cd1f49b7c18
Weslley Vieira <84158105+TLT-sptb@users.noreply.github.com>
2022-12-06 22:34:44 -0300

    Add files via upload

--- backend/controller/itensController.js
+++ backend/controller/itensController.js
@@ -4,23 +4,15 @@ const axios = require('axios');
 
 
 module.exports = class Analyzes{
-    static analises(req, res) {
+    static async analises(req, res) {
         let mlb = req.params.id; //Para uso de testes 'MLB1830777149''MLB1830777149'
         
         // if(!mlb.startsWith('MLB')) mlb = 'MLB'+ mlb;
-        
-       axios.get(`https://api.mercadolibre.com/items/${mlb}/variations`).then((response) => {
-                let data = response.data;
-
-                data.forEach(element => {
-                    const ele = element;
-                    let elementAtributes = element.attribute_combinations;
-                    elementAtributes.forEach(atributes => {
-                        const atrb = atributes;
-                })
-            })
-            res.json({message: data}).status(200).end();
-        }).catch((error) => res.status(400).json({ message: error }));
+       axios.get(`https://api.mercadolibre.com/items/${mlb}`).then((response) =>{
+           const data = response.data 
+           res.json(data).end();
+            // res.json(data.sale_terms.id);
+        }).catch(err => console.log(err));   
     }
     
 }
--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,7 +1,9 @@
 const User = require('../models/Users');
 const bcrypt = require('bcrypt');
-const axios = require('axios');
+const createUserToken = require('../helpers/create-user-token');
 const jwt = require('jsonwebtoken');
+const getToken = require('../helpers/get-token');
+
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -94,5 +96,34 @@ module.exports = class UserController {
             return;
         }
         res.redirect('/');
+    }
+    static async checkUser(req, res){
+        let currentUser;
+
+        if(req.headers.authorization){
+            const token = getToken(req);
+            const decoded = jwt.verify(token, '45&3NJIDdE2x');
+
+            currentUser = await User.findById(decoded.id)
+
+            currentUser.password = undefined
+        }else{
+            currentUser = null
+        }
+
+        res.status(200).send(currentUser);
+    }
+    static async getUserById(req, res){
+        const id = req.params.id;
+
+        const user = await user.findById(id).select('-password');
+
+        if(!user){
+            res.status(422).json({
+                message: 'Usuário não encontrado'
+            })
+            return
+        }
+        res.status(200).json({ user });
     }
 }
