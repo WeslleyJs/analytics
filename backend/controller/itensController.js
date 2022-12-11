57a5fc61e1356d7f94c614b6f56cc7fcbe0b640c
Weslley Vieira <84158105+TLT-sptb@users.noreply.github.com>
2022-12-11 17:26:12 -0300

    Add files via upload

--- backend/controller/itensController.js
+++ backend/controller/itensController.js
@@ -3,16 +3,31 @@ const axios = require('axios');
 
 
 
-module.exports = class Analyzes{
+module.exports = class Analyzes {
     static async analises(req, res) {
         let mlb = req.params.id; //Para uso de testes 'MLB1830777149''MLB1830777149'
-        
-        // if(!mlb.startsWith('MLB')) mlb = 'MLB'+ mlb;
-       axios.get(`https://api.mercadolibre.com/items/${mlb}`).then((response) =>{
-           const data = response.data 
-           res.json(data).end();
-            // res.json(data.sale_terms.id);
-        }).catch(err => console.log(err));   
+
+
+        if (!mlb.startsWith('MLB')) mlb = 'MLB' + mlb;
+        axios.get(`https://api.mercadolibre.com/items/${mlb}`).then((response) => {
+            const data = response.data
+            res.json(data).end();
+        }).catch((err) => {
+            const error = err.message
+            res.redirect('/')
+        });
     }
-    
+    static async shipping(req, res) {
+        let mlb = req.params.id;
+
+        if(!mlb.startsWith('MLB')) mlb = 'MLB'+mlb
+        await axios.get(`https://api.mercadolibre.com/items/shipping_options/free?ids=${mlb}`).then((response) => {
+            const data = response.data;
+
+            Object.keys(data).map(function(key){
+               const result = data[key].coverage.all_country
+                res.json(result);
+            })
+        }).catch(err => console.log(err));
+    }
 }
